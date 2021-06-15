a74a6aaadb5654d76b7fd40a976b287723257e0a
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-15 17:42:03 +0530

    Welcome to algorithm Programms

---
+++ AlgorithmProgramms/AlgorithmProgramms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmProgramms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to algorithm Programs");
+        }
+    }
+}
